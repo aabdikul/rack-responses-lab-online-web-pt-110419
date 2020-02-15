b30eaf9b4dcbfc1c3b9ecf1bc9bc6e436fae48aa
Aida Abdikulova <abdikulova@gmail.com>
2020-02-15 22:08:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,15 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new
+
+    t = Time.now
+    if t >  12
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end
 
   end
-  
+
 end
